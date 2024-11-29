6ffbfc6de4c73f54e0fdd39a691ec386d74ff492
lizard123137 <lizard123137@gmail.com>
2024-11-29 20:39:59 +0100

    minor update to network

--- src/neural_network.hpp
+++ src/neural_network.hpp
@@ -1,8 +1,34 @@
 #ifndef NEURAL_NETWORK_H
 #define NEURAL_NETWORK_H
 
+#include <Eigen/Dense>
+#include <vector>
+
+#include "neural_layer.hpp"
+
 class NeuralNetwork {
-    
+private:
+    std::vector<NeuralLayer> layers;
+
+public:
+    NeuralNetwork();
+
+    void addLayer(NeuralLayer layer);
+    Eigen::VectorXd predict(Eigen::VectorXd inputs) const;
 };
+
+void NeuralNetwork::addLayer(NeuralLayer layer) {
+    this->layers.push_back(layer);
+}
+
+Eigen::VectorXd NeuralNetwork::predict(Eigen::VectorXd inputs) const {
+    Eigen::VectorXd layerInput = inputs;
+
+    for(auto layer : this->layers) {
+        layerInput = layer.getPredictions(layerInput);
+    }
+
+    return layerInput;
+}
 
 #endif
